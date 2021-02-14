51823e2bb943f5e9e84dd91b62a064094afd1ba7
kuralin3 <asaki046-kun@yahoo.co.jp>
2021-02-14 11:30:05 +0900

    最初のコミット

---
+++ resources/views/management/category.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-4">
+        <div class="list-group">
+          <a href="/management/category" class="list-group-item">Category</a>
+          <a href="" class="list-group-item">Menu</a>
+          <a href="" class="list-group-item">Table</a>
+          <a href="" class="list-group-item">User</a>
+        </div>
+      </div>
+      <div class="col-md-8">
+          <h1>Category</h1>
+      </div>
+    </div>
+  </div>
+@endsection
